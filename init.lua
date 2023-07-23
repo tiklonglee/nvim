1e450c2a1787ce0b90b34940ba743464ee2c5f24
Tik Long Lee <leetiklong@gmail.com>
2023-07-23 19:31:33 +0800

    [set.lua] Common vim config (number, indentation, search, undo, etc...)

--- init.lua
+++ init.lua
@@ -13,3 +13,4 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = ","
 require("lazy").setup("plugin")
+require("core")
---
+++ lua/core/set.lua
@@ -0,0 +1,34 @@
+-- Set number
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Indentation Related
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.cino = 'N-s'
+
+-- Search Setting
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
+-- Undo Setting
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+-- Split Setting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Set various details
+vim.opt.autochdir = true
+vim.opt.scrolloff = 10
+vim.g.netrw_dirhistmax = 0
