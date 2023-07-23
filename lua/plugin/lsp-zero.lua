20fff7682c1e3c528443482d1972d5132b799a13
Tik Long Lee <leetiklong@gmail.com>
2023-07-23 20:05:19 +0800

    [lsp.lua] add lsp support via lsp-zero (for auto-completion + formatting + ...)

---
+++ lua/plugin/lsp-zero.lua
@@ -0,0 +1,18 @@
+return {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {                                      -- Optional
+            'williamboman/mason.nvim',
+            build = ':MasonUpdate'
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
+}
