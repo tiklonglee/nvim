8808ba33d5c95b14b07c356121983dd7ce5a27d8
Tik Long Lee <leetiklong@gmail.com>
2023-09-02 17:56:21 +0800

    [telescope.lua] change tab new mapping to tab drop

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,21 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        require('telescope').setup{
+            defaults = {
+                -- default configuration for telescope goes here:
+                -- config_key = value,
+                mappings = {
+                    i = {
+                        -- map actions.which_key to <C-h> (default: <C-/>)
+                        -- actions.which_key shows the mappings for your picker,
+                        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                        ["<C-t>"] = function(bufnr) require("telescope.actions.set").edit(bufnr, "tab drop") end
+                    }
+                }
+            },
+        }
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<C-p>',
             function()
